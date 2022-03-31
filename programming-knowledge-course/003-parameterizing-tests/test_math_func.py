db4addc2b19057a13f4f4d65e7463060385e72e9
Hediberto Cavalcante da Silva <hed.cavalcante@gmail.com>
2022-03-31 19:15:19 -0300

    Course ProgrammingKnowledge - Python Unit Testing With Pytest

---
+++ programming-knowledge-course/003-parameterizing-tests/test_math_func.py
@@ -0,0 +1,1 @@
+import math_funcimport pytest@pytest.mark.parametrize('x, y, result',                         [                             (1, 2, 3),                             ("Hello ", "World", "Hello World"),                             (1.0, 2.0, 3.0)                         ])def test_add(x, y, result):    assert math_func.add(x, y) == result
